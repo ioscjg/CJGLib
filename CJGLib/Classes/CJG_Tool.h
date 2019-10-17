7996fefaeee2df6a04413eba7216405f50fbde13
cjg <chijg@imscv.com>
2019-10-17 19:40:16 +0800

    ssss

---
+++ CJGLib/Classes/CJG_Tool.h
@@ -0,0 +1,17 @@
+//
+//  CJG_Tool.h
+//  CJGLib_Example
+//
+//  Created by cjg on 2019/10/17.
+//  Copyright © 2019 ioscjg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CJG_Tool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CJGLib/Classes/CJG_Tool.m
@@ -0,0 +1,13 @@
+//
+//  CJG_Tool.m
+//  CJGLib_Example
+//
+//  Created by cjg on 2019/10/17.
+//  Copyright © 2019 ioscjg. All rights reserved.
+//
+
+#import "CJG_Tool.h"
+
+@implementation CJG_Tool
+
+@end
